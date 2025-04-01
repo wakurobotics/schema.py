3fdd9298893d9ea9170d788403c67b8514bc161f
wakurobotics <tech@waku-robotics.com>
2025-04-01 06:05:47 +0000

    Update from https://github.com/wakurobotics/schema/commit/488ee64cf325d301491edb59bfb6e5599300b9dd

--- wakurobotics/care/devices/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-from .values_schema import DeviceValues
-from .shared.value_schema import ScalarValue
-from .factsheet_schema import DeviceFactsheet
-from .connection_schema import Connection
-from .order_schema import Order
-from .errorlog_schema import WakuMqttApiErrors
-from .error_schema import DeviceErrors
-
-__all__ = ['DeviceValues', 'ScalarValue', 'DeviceFactsheet', 'Connection', 'ConnectionStatus', 'Order', 'Status', 'WakuMqttApiErrors', 'DeviceErrors', 'Error']
